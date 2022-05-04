8d21d27bf3af17b8a14b27ff6287f69849df6142
Sourcery AI <>
2022-05-04 16:14:32 +0000

    'Refactored by Sourcery'

--- service/routes/routes.py
+++ service/routes/routes.py
@@ -14,5 +14,4 @@ async def get_movie(title_subtext: str) -> Optional[MovieModel]:
     results = data["hits"]
     if not results:
         return None
-    movie = MovieModel(**results[0])
-    return movie
+    return MovieModel(**results[0])
